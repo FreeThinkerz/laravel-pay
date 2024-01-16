1d3749442930505b99ef5233accae9b30870a7bd
Minister <mundestephane13@gmail.com>
2024-01-17 00:15:02 +0100

    Refactored the hasPayments trait

--- src/Helper/HasPayments.php
+++ src/Helper/HasPayments.php
@@ -29,20 +29,6 @@ trait HasPayments
      */
     public function payment(string $payer = null, float|int $amount = null, string $service = null): PaymentBuilder
     {
-        return new PaymentBuilder($this, $payer, $amount, $service ?? $this->getPaymentServiceFromPhone($payer));
-    }
-
-    /**
-     *  Get the payment service from from the phone number
-     */
-    public function getPaymentServiceFromPhone(string $phone): string | Exception
-    {
-        if (Str::match('/^(?:\+237|237)?6(?:5[0-4]|[87][0-9])\d{6}$/', $phone)) {
-            return 'MTN';
-        } elseif (Str::match('/^(?:\+237|237)?6(?:5[5-9]|[9][0-9])\d{6}$/', $phone)) {
-            return 'Orange';
-        } else {
-            throw new Exception("Invalid Payment Number: {$phone}");
-        }
+        return new PaymentBuilder($this, $payer, $amount);
     }
 }
